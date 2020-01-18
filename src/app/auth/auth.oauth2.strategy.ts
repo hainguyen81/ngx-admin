359422ec2f82b37575db52dc81489eeb42ddd700
[Nova] Nguyễn Long Hải <hai.nguyenlong@axonactive.com>
2020-01-19 00:18:24 +0700

    authenticate with mock data

--- src/app/auth/auth.oauth2.strategy.ts
+++ src/app/auth/auth.oauth2.strategy.ts
@@ -47,7 +47,7 @@ export class NbxOAuth2AuthStrategy extends NbPasswordAuthStrategy {
     let _this = this;
     this.authHttpService.setCreateTokenDelegate(
       function(value: any, failWhenInvalidToken?: boolean) {
-        return _this.createToken(value, failWhenInvalidToken);
+        return _this.createToken(_this, value, failWhenInvalidToken);
       });
     authDbService || throwError('Could not inject IndexedDb!');
     logger || throwError('Could not inject logger!');
@@ -75,8 +75,8 @@ export class NbxOAuth2AuthStrategy extends NbPasswordAuthStrategy {
     return this.getHttpService().request(url, method, options);
   }
 
-  createToken<T extends NbAuthToken>(value: any, failWhenInvalidToken?: boolean): T {
-    return this.storeDb(super.createToken(value, this.getOption(`login.failWhenInvalidToken`)));
+  createToken<T extends NbAuthToken>(assigner: NbxOAuth2AuthStrategy, value: any, failWhenInvalidToken?: boolean): T {
+    return assigner.storeDb(assigner.createToken(value, assigner.getOption(`login.failWhenInvalidToken`)));
   }
 
   private storeDb<T extends NbAuthToken>(token?: T): T {
