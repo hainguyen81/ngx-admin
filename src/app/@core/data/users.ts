312654e97ed6ba318186c52abd5054f34c4f4162
[Nova] Nguyễn Long Hải <hai.nguyenlong@axonactive.com>
2020-01-20 21:58:21 +0700

    remove all example components

--- src/app/@core/data/users.ts
+++
@@ -1,6 +0,0 @@
-import { Observable } from 'rxjs';
-import {User} from './user';
-
-export abstract class UserData {
-  abstract getUsers(): Observable<User[]>;
-}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {Injector, NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
-import {CoreModule} from './@core/core.module';
 import {ThemeModule} from './@theme/theme.module';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
@@ -56,7 +55,6 @@ import {ToastrModule} from 'ngx-toastr';
     NbChatModule.forRoot({
       messageGoogleMapKey: 'AIzaSyA_wNuCzia92MAmdLRzmqitRGvCF7wCZPY',
     }),
-    CoreModule.forRoot(),
 
     /* Toaster */
     ToastrModule.forRoot(AppConfig.TOASTER),
