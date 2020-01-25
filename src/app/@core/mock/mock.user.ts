eb164da9ececd5d00fba4ace96fc643f4e426a73
[Nova] Nguyễn Long Hải <hai.nguyenlong@axonactive.com>
2020-01-25 16:14:41 +0700

    fix unique mock data

--- src/app/@core/mock/mock.user.ts
+++ src/app/@core/mock/mock.user.ts
@@ -20,9 +20,9 @@ export const MockUserAdmin: IUser = {
     rolesGroupId: '5d766194738fbc23b43a857f',
 };
 export const MockUser1: IUser = {
-    access_token: 'f90087e0-398e-4124-9522-5a4e6f0bed91',
+    access_token: 'f90087e0-398e-4124-9522-5a4e6f0bed92',
     token_type: 'bearer',
-    refresh_token: 'ac581551-9dd4-404a-8a4a-ecf591ebb9d0',
+    refresh_token: 'ac581551-9dd4-404a-8a4a-ecf591ebb9d1',
     expires_in: 7889236,
     scope: 'READ WRITE',
     company: 'hsg',
@@ -34,13 +34,13 @@ export const MockUser1: IUser = {
     email: 'user1@hsg.com',
     status: USER_STATUS.ACTIVATED,
     rolesGroup: MockRolesGroupSystem,
-    id: '5d5d67786249ab06b4516c45',
+    id: '5d5d67786249ab06b4516c46',
     rolesGroupId: '5d766194738fbc23b43a857f',
 };
 export const MockUser2: IUser = {
-    access_token: 'f90087e0-398e-4124-9522-5a4e6f0bed91',
+    access_token: 'f90087e0-398e-4124-9522-5a4e6f0bed93',
     token_type: 'bearer',
-    refresh_token: 'ac581551-9dd4-404a-8a4a-ecf591ebb9d0',
+    refresh_token: 'ac581551-9dd4-404a-8a4a-ecf591ebb9d2',
     expires_in: 7889236,
     scope: 'READ WRITE',
     company: 'hsg',
@@ -52,7 +52,7 @@ export const MockUser2: IUser = {
     email: 'user2@hsg.com',
     status: USER_STATUS.ACTIVATED,
     rolesGroup: MockRolesGroupSystem,
-    id: '5d5d67786249ab06b4516c45',
+    id: '5d5d67786249ab06b4516c47',
     rolesGroupId: '5d766194738fbc23b43a857f',
 };
 
