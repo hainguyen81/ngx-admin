7ef244a3c2aacefedb188a0309d4d6ab9d06d8f1
[Nova] Nguyễn Long Hải <hai.nguyenlong@axonactive.com>
2020-02-04 21:15:11 +0700

    add UUID, OBjectID generators and generate large mock users, customers

--- src/app/config/generator.config.ts
+++ src/app/config/generator.config.ts
@@ -1,7 +1,6 @@
 export const IdGenerators = {
     uuid: {
         v1: require('uuid/v1'),
-        v2: require('uuid/v2'),
         v3: require('uuid/v3'),
         v4: require('uuid/v4'),
         v5: require('uuid/v5'),
