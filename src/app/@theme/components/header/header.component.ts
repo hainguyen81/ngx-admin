ff188821096d2811a4704d5b916d4214a79812d5
[Nova] Nguyễn Long Hải <hai.nguyenlong@axonactive.com>
2020-02-16 21:36:06 +0700

    add languages selection

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -1,103 +1,132 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { NbMediaBreakpointsService, NbMenuService, NbSidebarService, NbThemeService } from '@nebular/theme';
-import { map, takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {NbMediaBreakpointsService, NbMenuService, NbSidebarService, NbThemeService} from '@nebular/theme';
+import {map, takeUntil} from 'rxjs/operators';
+import {Subject} from 'rxjs';
 /* Authentication */
-import { NbAuthOAuth2Token, NbAuthService } from '@nebular/auth';
+import {NbAuthOAuth2Token, NbAuthService} from '@nebular/auth';
+import {AppConfig} from '../../../config/app.config';
+import {TranslateService} from '@ngx-translate/core';
+import {NGXLogger} from 'ngx-logger';
 
 @Component({
-  selector: 'ngx-header',
-  styleUrls: ['./header.component.scss'],
-  templateUrl: './header.component.html',
+    selector: 'ngx-header',
+    styleUrls: ['./header.component.scss'],
+    templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit, OnDestroy {
 
-  private destroy$: Subject<void> = new Subject<void>();
-  userPictureOnly: boolean = false;
+    private destroy$: Subject<void> = new Subject<void>();
+    userPictureOnly: boolean = false;
 
-  /* Authentication */
-  user: any;
-
-  themes = [
-    {
-      value: 'default',
-      name: 'Light',
-    },
-    {
-      value: 'dark',
-      name: 'Dark',
-    },
-    {
-      value: 'cosmic',
-      name: 'Cosmic',
-    },
-    {
-      value: 'corporate',
-      name: 'Corporate',
-    },
-  ];
-
-  currentTheme = 'default';
-
-  userMenu = [ { title: 'Profile' }, { title: 'Log out' } ];
-
-  constructor(private sidebarService: NbSidebarService,
-              private menuService: NbMenuService,
-              private themeService: NbThemeService,
-              private breakpointService: NbMediaBreakpointsService,
-              private authService: NbAuthService) {
     /* Authentication */
-    this.authService.onTokenChange()
-      .subscribe((token: NbAuthOAuth2Token) => {
+    user: any;
 
-        if (token.isValid()) {
-          // here we receive a payload from the token and assigns it to our `user` variable
-          this.user = token.getPayload();
+    languages = AppConfig.i18n.languages;
+
+    themes = [
+        {
+            value: 'default',
+            name: 'Light',
+        },
+        {
+            value: 'dark',
+            name: 'Dark',
+        },
+        {
+            value: 'cosmic',
+            name: 'Cosmic',
+        },
+        {
+            value: 'corporate',
+            name: 'Corporate',
+        },
+    ];
+
+    currentTheme = 'default';
+    currentLang = 'en';
+
+    userMenu = [{title: 'Profile'}, {title: 'Log out'}];
+
+    constructor(private sidebarService: NbSidebarService,
+                private menuService: NbMenuService,
+                private themeService: NbThemeService,
+                private breakpointService: NbMediaBreakpointsService,
+                private authService: NbAuthService,
+                private translateService: TranslateService,
+                private logger: NGXLogger) {
+        /* Authentication */
+        this.authService.onTokenChange()
+            .subscribe((token: NbAuthOAuth2Token) => {
+
+                if (token.isValid()) {
+                    // here we receive a payload from the token and assigns it to our `user` variable
+                    this.user = token.getPayload();
+                    this.currentLang = this.user['lang'] || this.languages[0];
+                    this.user['lang'] = this.currentLang;
+                }
+            });
+    }
+
+    ngOnInit() {
+        this.currentTheme = this.themeService.currentTheme;
+        this.languages = this.translateService.getLangs();
+        if (!this.languages || !this.languages.length) {
+            this.languages = [];
+            (this.translateService.defaultLang || '').length
+            && this.languages.push(this.translateService.defaultLang);
         }
+        let currentLang: string;
+        currentLang = ((this.translateService.currentLang || '').length
+            ? this.translateService.currentLang
+            : (this.translateService.defaultLang || '').length
+                ? this.translateService.defaultLang
+                : this.languages && this.languages.length ? this.languages[0] : '');
+        (this.languages.indexOf(currentLang) < 0)
+        && this.languages.push(currentLang);
+        this.currentLang = currentLang;
+        this.logger.debug('CURRENT LANGUAGE', this.currentLang);
 
-      });
-  }
+        /*this.userService.getUsers()
+          .pipe(takeUntil(this.destroy$))
+          .subscribe((users: any) => this.user = users.nick);*/
 
-  ngOnInit() {
-    this.currentTheme = this.themeService.currentTheme;
+        const {xl} = this.breakpointService.getBreakpointsMap();
+        this.themeService.onMediaQueryChange()
+            .pipe(
+                map(([, currentBreakpoint]) => currentBreakpoint.width < xl),
+                takeUntil(this.destroy$),
+            )
+            .subscribe((isLessThanXl: boolean) => this.userPictureOnly = isLessThanXl);
 
-    /*this.userService.getUsers()
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((users: any) => this.user = users.nick);*/
+        this.themeService.onThemeChange()
+            .pipe(
+                map(({name}) => name),
+                takeUntil(this.destroy$),
+            )
+            .subscribe(themeName => this.currentTheme = themeName);
+    }
 
-    const { xl } = this.breakpointService.getBreakpointsMap();
-    this.themeService.onMediaQueryChange()
-      .pipe(
-        map(([, currentBreakpoint]) => currentBreakpoint.width < xl),
-        takeUntil(this.destroy$),
-      )
-      .subscribe((isLessThanXl: boolean) => this.userPictureOnly = isLessThanXl);
+    ngOnDestroy() {
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
 
-    this.themeService.onThemeChange()
-      .pipe(
-        map(({ name }) => name),
-        takeUntil(this.destroy$),
-      )
-      .subscribe(themeName => this.currentTheme = themeName);
-  }
+    changeTheme(themeName: string) {
+        this.themeService.changeTheme(themeName);
+    }
 
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
+    changeLanguage(language: string) {
+        this.translateService.use(language);
+    }
 
-  changeTheme(themeName: string) {
-    this.themeService.changeTheme(themeName);
-  }
+    toggleSidebar(): boolean {
+        this.sidebarService.toggle(true, 'menu-sidebar');
 
-  toggleSidebar(): boolean {
-    this.sidebarService.toggle(true, 'menu-sidebar');
+        return false;
+    }
 
-    return false;
-  }
-
-  navigateHome() {
-    this.menuService.navigateHome();
-    return false;
-  }
+    navigateHome() {
+        this.menuService.navigateHome();
+        return false;
+    }
 }
