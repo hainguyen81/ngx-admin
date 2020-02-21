783264c84d561b0716cdff9f344600edc86ca405
Hai Nguyen <hainguyen81@users.noreply.github.com>
2020-02-21 20:22:34 +0700

    add tabset component

---
+++ src/app/pages/components/tab/tab.component.ts
@@ -0,0 +1,96 @@
+import {
+    AfterViewInit,
+    ChangeDetectorRef,
+    Component,
+    ComponentFactoryResolver,
+    Inject,
+    QueryList,
+    Renderer2,
+    ViewChildren,
+    ViewContainerRef,
+} from '@angular/core';
+import {DataSource} from 'ng2-smart-table/lib/data-source/data-source';
+import {ContextMenuService} from 'ngx-contextmenu';
+import {NGXLogger} from 'ngx-logger';
+import {TranslateService} from '@ngx-translate/core';
+import ComponentUtils from '../../../utils/component.utils';
+import {ToastrService} from 'ngx-toastr';
+import {ModalDialogService} from 'ngx-modal-dialog';
+import {ConfirmPopup} from 'ngx-material-popup';
+import {AbstractTabComponent} from './abstract.tab.component';
+
+/**
+ * SplitPane component base on {NbTabsetModule}
+ */
+@Component({
+    selector: 'ngx-tabset',
+    templateUrl: './tab.component.html',
+    styleUrls: ['./tab.component.scss'],
+})
+export class NgxTabsetComponent extends AbstractTabComponent<DataSource> implements AfterViewInit {
+
+    // -------------------------------------------------
+    // DECLARATION
+    // -------------------------------------------------
+
+    @ViewChildren('tabContentHolder', {read: ViewContainerRef})
+    private readonly queryTabContentHolderViewContainerRefs: QueryList<ViewContainerRef>;
+    private tabContentHolderViewContainerRefs: ViewContainerRef[];
+
+    // -------------------------------------------------
+    // GETTERS/SETTERS
+    // -------------------------------------------------
+
+    /**
+     * Get the {ViewContainerRef} instances array of {NbTabComponent}
+     * @return the {ViewContainerRef} instances array of {NbTabComponent}
+     */
+    protected getTabContentHolderViewContainerComponents(): ViewContainerRef[] {
+        return this.tabContentHolderViewContainerRefs;
+    }
+
+    // -------------------------------------------------
+    // CONSTRUCTION
+    // -------------------------------------------------
+
+    /**
+     * Create a new instance of {NgxTabsetComponent} class
+     * @param dataSource {DataSource}
+     * @param contextMenuService {ContextMenuService}
+     * @param toasterService {ToastrService}
+     * @param logger {NGXLogger}
+     * @param renderer {Renderer2}
+     * @param translateService {TranslateService}
+     * @param factoryResolver {ComponentFactoryResolver}
+     * @param viewContainerRef {ViewContainerRef}
+     * @param changeDetectorRef {ChangeDetectorRef}
+     * @param modalDialogService {ModalDialogService}
+     * @param confirmPopup {ConfirmPopup}
+     */
+    constructor(@Inject(DataSource) dataSource: DataSource,
+                @Inject(ContextMenuService) contextMenuService: ContextMenuService,
+                @Inject(ToastrService) toasterService: ToastrService,
+                @Inject(NGXLogger) logger: NGXLogger,
+                @Inject(Renderer2) renderer: Renderer2,
+                @Inject(TranslateService) translateService: TranslateService,
+                @Inject(ComponentFactoryResolver) factoryResolver: ComponentFactoryResolver,
+                @Inject(ViewContainerRef) viewContainerRef: ViewContainerRef,
+                @Inject(ChangeDetectorRef) changeDetectorRef: ChangeDetectorRef,
+                @Inject(ModalDialogService) modalDialogService?: ModalDialogService,
+                @Inject(ConfirmPopup) confirmPopup?: ConfirmPopup) {
+        super(dataSource, contextMenuService, toasterService, logger,
+            renderer, translateService, factoryResolver,
+            viewContainerRef, changeDetectorRef,
+            modalDialogService, confirmPopup,
+            1, true, true);
+    }
+
+    ngAfterViewInit(): void {
+        super.ngAfterViewInit();
+
+        if (!this.tabContentHolderViewContainerRefs || !this.tabContentHolderViewContainerRefs.length) {
+            this.tabContentHolderViewContainerRefs =
+                ComponentUtils.queryComponents(this.queryTabContentHolderViewContainerRefs);
+        }
+    }
+}
