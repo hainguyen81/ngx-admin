4034ec353ec0c4d5480bb2029194e2b29bf2306e
Hai Nguyen <hainguyen81@users.noreply.github.com>
2020-04-22 23:49:33 +0700

    add warehouse master settings

---
+++ src/app/@core/data/constants/warehouse.settings.constants.ts
@@ -0,0 +1,12 @@
+export namespace Constants {
+    export namespace WarehouseSettingsConstants {
+        export const enum SETTINGS_TYPE {
+            STATUS,
+            BRAND,
+            COLOR,
+            SIZE,
+            MATERIAL,
+            OTHERS,
+        }
+    }
+}
---
+++ src/app/@core/data/warehouse/warehouse.setting.ts
@@ -0,0 +1,17 @@
+import BaseModel, {IModel} from '../base';
+import {Constants} from '../constants/warehouse.settings.constants';
+import SETTINGS_TYPE = Constants.WarehouseSettingsConstants.SETTINGS_TYPE;
+
+export interface IWarehouseSetting extends IModel {
+    code: string;
+    name: string;
+    type?: SETTINGS_TYPE | SETTINGS_TYPE.OTHERS;
+    order?: number | 0;
+    remark?: string | null;
+}
+
+export default class WarehouseSettings extends BaseModel implements IWarehouseSetting {
+    constructor(public id: string, public code: string, public name: string) {
+        super(id);
+    }
+}
