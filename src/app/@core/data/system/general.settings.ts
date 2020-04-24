a9621e5db4ca7715a239b6be0d17d3047d43f538
Hai Nguyen <hainguyen81@users.noreply.github.com>
2020-04-24 09:06:45 +0700

    general settings master

---
+++ src/app/@core/data/system/general.settings.ts
@@ -0,0 +1,16 @@
+import BaseModel, {IModel} from '../base';
+import {IModule} from './module';
+
+export interface IGeneralSettings extends IModel {
+    code: string;
+    name: string;
+    value: number | string;
+    module_id?: string | null;
+    module?: IModule | null;
+}
+
+export default class GeneralSettings extends BaseModel implements IGeneralSettings {
+    constructor(public id: string, public code: string, public name: string, public value: number | string) {
+        super(id);
+    }
+}
