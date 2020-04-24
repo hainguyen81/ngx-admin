ffa42bc721bd0861025a9dfce049af483c522538
Hai Nguyen <hainguyen81@users.noreply.github.com>
2020-04-24 15:20:09 +0700

    restructure API/modules

--- src/app/utils/system/api.mapper.utils.ts
+++ src/app/utils/system/api.mapper.utils.ts
@@ -1,18 +1,70 @@
 import {API} from '../../config/api.config';
+import {isNullOrUndefined, isObject} from 'util';
 
 /**
  * API client link mapper utilities
  */
 export default class ApiMapperUtils {
+    /**
+     * Get the client route link by the specified API code
+     * @param apiCode to filter
+     * @return the client route link
+     */
     public static findClientLink(apiCode?: string): string {
-        if (!apiCode) {
-            return '';
+        let clientLink: string;
+        clientLink = '';
+        if (!(apiCode || '').length) {
+            return clientLink;
         }
         for (const api of Object.values(API)) {
-            if (api && api.hasOwnProperty('code') && api['code'] === apiCode) {
-                return api['client'] || '';
+            clientLink = this.findClientLinkRecursive(apiCode, api);
+            if (!isNullOrUndefined(clientLink)) {
+                break;
             }
         }
-        return '';
+        return (clientLink || '');
+    }
+    private static findClientLinkRecursive(code?: string | null, api?: any | null): string {
+        if (!(code || '').length || !api || !isObject(api)) {
+            return null;
+        }
+
+        // parse API code
+        let apiCode: string;
+        apiCode = '';
+        if (api.hasOwnProperty('code')) {
+            if (typeof api['code'] === 'function') {
+                apiCode = api['code']['call'](undefined) as string;
+            } else {
+                apiCode = api['code'] as string;
+            }
+        }
+
+        // parse API client link
+        let clientLink: string;
+        clientLink = undefined;
+        if (api.hasOwnProperty('client')
+            && isObject(api['client']) && (<Object>api['client']).hasOwnProperty('url')) {
+            if (typeof api['client']['url'] === 'function') {
+                clientLink = api['client']['url']['call'](undefined) as string;
+            } else {
+                clientLink = api['client']['url'] as string;
+            }
+        }
+
+        if (apiCode === code) {
+            return (clientLink || '');
+        }
+
+        if (api.hasOwnProperty('children') && isObject(api['children'])) {
+            clientLink = undefined;
+            for (const [k, v] of Object.entries(<Object>api['children'])) {
+                clientLink = this.findClientLinkRecursive(code, v);
+                if (!isNullOrUndefined(clientLink)) {
+                    break;
+                }
+            }
+        }
+        return clientLink;
     }
 }
