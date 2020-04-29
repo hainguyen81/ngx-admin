a8d275f8ea0fc499d5aa60a9f4d197dfd3b743a9
Hai Nguyen <hainguyen81@users.noreply.github.com>
2020-04-29 18:12:52 +0700

    add warehouse item brand settings

--- src/app/@core/data/constants/warehouse.category.constants.ts
+++ src/app/@core/data/constants/warehouse.category.constants.ts
@@ -6,16 +6,12 @@ export namespace Constants {
                 CATEGORY = 'common.enum.warehouseCategoryType.category',
                 // Loại
                 TYPE = 'common.enum.warehouseCategoryType.type',
-                // Hãng sản xuất
-                BRAND = 'common.enum.warehouseCategoryType.brand',
             }
 
             export function convertWarehouseCategoryTypeToDisplay(value: CATEGORY_TYPE): string {
                 switch (value) {
                     case CATEGORY_TYPE.TYPE:
                         return 'common.enum.warehouseCategoryType.type';
-                    case CATEGORY_TYPE.BRAND:
-                        return 'common.enum.warehouseCategoryType.brand';
                     default:
                         return 'common.enum.warehouseCategoryType.category';
                 }
