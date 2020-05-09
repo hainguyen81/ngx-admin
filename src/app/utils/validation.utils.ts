7e0a0fd85eb78acf2922b46e1f8b8d7b82f121de
Hai Nguyen <hainguyen81@users.noreply.github.com>
2020-05-09 14:56:04 +0700

    validate code

--- src/app/utils/validation.utils.ts
+++ src/app/utils/validation.utils.ts
@@ -1,5 +1,5 @@
 export default class ValidationUtils {
 
-    public static VALIDATION_CODE_PATTERN: RegExp = /[a-zA-Z0-9_-]{4,16}$/g;
+    public static VALIDATION_CODE_PATTERN: RegExp = /^(?=^.{12}$)[A-Z]{1,2}\d*$/g;
     public static VALIDATION_PASSWORD_PATTERN: RegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{6,12}$/g;
 }
