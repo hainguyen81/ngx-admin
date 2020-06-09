0d658d1a3bc214f47928d96781337e5d5509129f
[Nova] Nguyễn Long Hải <hai.nguyenlong@axonactive.com>
2020-06-09 10:58:00 +0700

    try service worker

---
+++ src/assets/workers/warehouse/warehouse.inventory/warehouse.inventory.service.worker.js
@@ -0,0 +1,13 @@
+importScripts('./assets/workers/libs/idbstore/1.7.2/idbstore.min.js')
+
+var inventories = new IDBStore({
+    dbVersion: 1,
+    storeName: 'warehouse_inventory',
+    keyPath: 'id',
+    autoIncrement: true,
+    onStoreReady: function(){
+        console.log('Store ready!');
+    }
+});
+
+
