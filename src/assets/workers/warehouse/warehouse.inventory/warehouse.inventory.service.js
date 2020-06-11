0544a99bebd9e048e8e03cfe9db4b935535b6b25
[Nova] Nguyễn Long Hải <hai.nguyenlong@axonactive.com>
2020-06-11 18:09:27 +0700

    try service worker

--- src/assets/workers/warehouse/warehouse.inventory/warehouse.inventory.service.js
+++ src/assets/workers/warehouse/warehouse.inventory/warehouse.inventory.service.js
@@ -25,60 +25,45 @@ class WarehouseInventoryServiceWorkerDatabase extends ServiceWorkerDatabase {
             var invDetailStore = transaction.objectStore(dbStores.warehouse_inventory_detail);
             var wmStore = transaction.objectStore(dbStores.warehouse_management);
 
-            // recalculate INPUT
-            if (!inventory || inventory.type === 'common.enum.warehouseInventoryType.in') {
-                var cursor = null;
-                var index = null;
-                if (inventory) {
-                    index = invStore.index('code');
-                    cursor = index.openCursor(IDBKeyRange.only([inventory.code]));
+            var cursor = null;
+            if (inventory) {
+                var index = invStore.index('code');
+                cursor = index.openCursor(IDBKeyRange.only([inventory.code]));
+
+            } else {
+                cursor = invStore.openCursor();
+            }
+            cursor.onsuccess = function(e) {
+                var ecur = e.target.result;
+                if (ecur) {
+
+
+                    ecur.continue();
 
                 } else {
-                    index = invStore.index('type');
-                    cursor = index.openCursor(IDBKeyRange.only(['common.enum.warehouseInventoryType.in']));
+                    console.warn(`${_this.options.name}: Finish WAREHOUSE_INVENTORY calculation`);
                 }
-                cursor.onsuccess = function(e) {
-                    var ecur = e.target.result;
-                    if (ecur) {
-
-
-                        cursor.continue();
-
-                    } else {
-                        console.warn(`${_this.options.name}: Finish WAREHOUSE_INVENTORY calculation`);
-                    }
-                };
-                _this.recalculateInput(invStore, invDetailStore, wmStore, inventory, details);
-            }
-
-            // recalculate OUTPUT
-            if (!inventory || inventory.type === 'common.enum.warehouseInventoryType.out') {
-                _this.recalculateOutput(invStore, invDetailStore, wmStore, inventory, details);
-            }
-        })
+            };
+        });
     }
 
     /**
      * Recalculate the INPUT inventory quantities
-     * @param invStore `warehouse_inventory` database store
-     * @param invDetailStore `warehouse_inventory_detail` database store
      * @param wmStore `warehouse_management` database store
      * @param inventory to recalculate
      * @param details to recalculate
      */
-    recalculateInput(invStore, invDetailStore, wmStore, inventory, details) {
+    recalculateInput(wmStore, inventory, details) {
 
     }
 
     /**
      * Recalculate the OUTPUT inventory quantities
-     * @param invStore `warehouse_inventory` database store
-     * @param invDetailStore `warehouse_inventory_detail` database store
      * @param wmStore `warehouse_management` database store
      * @param inventory to recalculate
      * @param details to recalculate
      */
-    recalculateOutput(invStore, invDetailStore, wmStore, inventory, details) {
+    recalculateOutput(wmStore, inventory, details) {
 
     }
 }
