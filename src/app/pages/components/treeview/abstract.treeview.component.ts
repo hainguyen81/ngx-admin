1b1946d2e9e3da18216ae11126ddd67042a551d3
Hai Nguyen <hainguyen81@users.noreply.github.com>
2020-06-18 22:36:54 +0700

    fix warehouse for children

--- src/app/pages/components/treeview/abstract.treeview.component.ts
+++ src/app/pages/components/treeview/abstract.treeview.component.ts
@@ -49,6 +49,7 @@ import {isArray, isNullOrUndefined} from 'util';
  */
 export class NgxTreeviewConfig extends TreeviewConfig {
 
+    hasDivider: boolean | false;
     dropdown?: boolean | false;
     itemBuilder?: (data: any) => TreeviewItem | null;
     treeBuilder?: (data: any[]) => TreeviewItem[] | null;
@@ -64,6 +65,7 @@ export class NgxTreeviewConfig extends TreeviewConfig {
         hasCollapseExpand?: boolean | true;
         decoupleChildFromParent?: boolean | false;
         maxHeight?: number | 500;
+        hasDivider?: boolean | false;
         dropdown?: boolean | false;
         itemBuilder?: (data: any) => TreeviewItem | null;
         treeBuilder?: (data: any[]) => TreeviewItem[] | null;
@@ -83,6 +85,7 @@ export class NgxTreeviewConfig extends TreeviewConfig {
 
 /* default tree-view config */
 export const DefaultTreeviewConfig: NgxTreeviewConfig = NgxTreeviewConfig.create({
+    hasDivider: false,
     hasAllCheckBox: false,
     hasCollapseExpand: true,
     hasFilter: false,
