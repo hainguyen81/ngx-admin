98ea6990c308d405bb4d3c873ab28af64ad82209
hainguyen81 <hainguyen81@users.noreply.github.com>
2021-01-11 00:53:56 +0700

    upgrade angular from 8.2.14 to 9.1.13

--- src/app/config/generator.config.ts
+++ src/app/config/generator.config.ts
@@ -1,10 +1,10 @@
-export const uuid_dist = 'uuid/';
+import { v1 as uuidv1, v3 as uuidv3, v4 as uuidv4, v5 as uuidv5 } from 'uuid';
 export const IdGenerators = {
     uuid: {
-        v1: require(uuid_dist + 'v1'),
-        v3: require(uuid_dist + 'v3'),
-        v4: require(uuid_dist + 'v4'),
-        v5: require(uuid_dist + 'v5'),
+        v1: uuidv1,
+        v3: uuidv3,
+        v4: uuidv4,
+        v5: uuidv5,
     },
     oid: require('bson-objectid'),
 };
